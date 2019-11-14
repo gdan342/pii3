7e722d7b37d36304764da5a8288811d6c879e115
gdan342 <45246098+gdan342@users.noreply.github.com>
2019-11-14 21:46:45 +0000

    Update pii3install.sh

--- pii3install.sh
+++ pii3install.sh
@@ -27,7 +27,7 @@ echo '        Updated: 11/14/2019'
 echo '     Updating......'
 sudo apt-get update --yes
 echo '     Upgrading......'
-sudo apt-get Upgrade --yes
+sudo apt-get upgrade --yes
 set -e
 
 # install x server
@@ -38,7 +38,6 @@ set -e
 
 #create xinitrc
 echo '     Creating .xinitrc......'
-sudo touch ~/.xinitrc
 echo "#!/bin/bash" >> ~/.xinitrc
 echo "exec i3" > ~/.xinitrc
 set -e
@@ -56,17 +55,31 @@ echo '   |  _____)   | |      | |     (___ ('
 echo '   | (         | |      | |         ) \'
 echo '   | )      ___) (______) (___/\___/  /'
 echo '   |/       \_______/\_______/\______/'
-
+echo 
+echo
+echo 
 # install basic tools
 echo '     Installing Dependancies and Basic Tools......'
-sudo apt-get install leafpad htop python-pip ranger neofetch pcmanfm feh tmux links tty-clock wget suckless-tools lxterminal dmenu --yes
+sudo apt-get install leafpad htop python-pip ranger neofetch pcmanfm feh tmux links tty-clock wget suckless-tools lxterminal --yes
 set -e
 
 # install python packages
 echo '     Installing Python Tools......'
 sudo pip install --upgrade pip
-pip install pywal lolcat
+#pip install
 set -e
+
+echo '    _______ __________________ ______'
+echo '   (  ____ )\__   __/\__   __// ___  \'
+echo '   | (    )|   ) (      ) (   \/   \  \'
+echo '   | (____)|   | |      | |      ___) /'
+echo '   |  _____)   | |      | |     (___ ('
+echo '   | (         | |      | |         ) \'
+echo '   | )      ___) (______) (___/\___/  /'
+echo '   |/       \_______/\_______/\______/'
+echo 
+echo
+echo 
 
 echo '     Replacing config Files......'
 
@@ -103,4 +116,4 @@ sudo echo "bash ~/pii3/ufetch//ufetch-raspbian" >> ~/.bashrc
 
 # Reboot
 echo '      Rebooting System......'
-reboot
+sudo reboot -h 0
